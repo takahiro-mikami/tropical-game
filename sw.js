19952707d6085deaa953b8eb680e57a1283d0d06
takahiro-mikami <mikami@essence-corporation.com>
2026-06-03 22:16:17 +0900

    miss

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tropical-v1';
+const CACHE = 'tropical-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.svg', './icon-512.svg'];
 
 self.addEventListener('install', e => {
